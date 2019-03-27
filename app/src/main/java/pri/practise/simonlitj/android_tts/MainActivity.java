a162a116ae15441432e10376da21ac3fcffb9a75
ShimingLee <13132549866@163.com>
2019-03-27 11:32:37 +0800

    Initial commit

---
+++ app/src/main/java/pri/practise/simonlitj/android_tts/MainActivity.java
@@ -0,0 +1,13 @@
+package pri.practise.simonlitj.android_tts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
